81823e4f3b039f5b1ddeb3628845d529a9053d66
WJFil <greg.kitchen7@gmail.com>
2023-04-15 15:17:22 -0600

    user routes in userRoutes.js

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
+const courseRoutes = require('./thoughtRoutes');
+const studentRoutes = require('./userRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
+router.use('/thoughts', courseRoutes);
+router.use('/users', studentRoutes);
 
 module.exports = router;

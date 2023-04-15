c496dbe2c80f6db15277c42657f31e0a18160cac
WJFil <greg.kitchen7@gmail.com>
2023-04-15 14:57:50 -0600

    initial config, routes, model files

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/i,
+    },
+
+    thoughts: [thoughtSchema],
+    friends:[userSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+//Create a virtual property called friendCount that retrieves the length of the user's friends array field on query.
+
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  })
+
+const User = model('user', userSchema);
+
+module.exports = User;
